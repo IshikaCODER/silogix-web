7284ed4f3fda3f00d268206706cbcaf6ffc518fe
Ishika <ishika7473@gmail.com>
2024-07-31 11:33:10 +0530

    Modifications in routing

--- src/App.js
+++ src/App.js
@@ -1,51 +1,26 @@
-import Home from '../src/pages/Home'
-import Company from '../src/pages/Company'
-import Service from '../src/pages/Service'
-import Career from '../src/pages/Career'
-import Blog from '../src/pages/Blog'
-import BlogDescription from '../src/pages/BlogDescription'
-import Contact from '../src/pages/Contact'
-import Privacy from '../src/pages/Privacy'
-import Terms from '../src/pages/Terms'
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
-import FAQ from './pages/FAQs'
+import routes from './routes';
+import ScrollTop from './components/third-party/ScrollTop';
+
 
 function App() {
   return (
     <BrowserRouter>
     <Navbar/>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/company" element={<Company/>}/>
-        <Route path="/service" element={<Service/>}/>
-        <Route path="/career" element={<Career/>}/>
-        <Route path="/blog" element={<Blog/>}/>
-        <Route path="/blogdescription" element={<BlogDescription/>}/>
-        <Route path="/contact" element={<Contact/>}/>
-        <Route path="/privacy" element={<Privacy/>}/>
-        <Route path="/terms" element={<Terms/>}/>
-        <Route path="/faqs" element={<FAQ/>}/>
+    <ScrollTop>
+    <Routes>
+        {routes.map((route, index) => (
+          <Route key={index} path={route.path} element={route.element} />
+        ))}
       </Routes>
+      </ScrollTop>
       <Footer/>
     </BrowserRouter>
   );
 }
 
+
 export default App;
-
-
-
-// // project import
-// import Routes from './routes';
-
-// const App = () => (
-//   <>
-//   console.log('App component rendered');
-//   <Routes />
-//   </>
-// );
-
-// export default App;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,25 @@
-import { useRoutes } from 'react-router-dom';
+import Home from "../pages/Home";
+import Company from "../pages/Company";
+import Service from "../pages/Service";
+import Career from "../pages/Career";
+import Blog from "../pages/Blog";
+import BlogDescription from "../pages/BlogDescription";
+import Contact from "../pages/Contact";
+import Privacy from "../pages/Privacy";
+import Terms from "../pages/Terms";
+import FAQ from "../pages/FAQs";
 
-// project import
-import MainRoutes from './MainRoutes';
+const routes = [
+  { path: "/", element: <Home /> },
+  { path: "/company", element: <Company /> },
+  { path: "/service", element: <Service /> },
+  { path: "/career", element: <Career /> },
+  { path: "/blog", element: <Blog /> },
+  { path: "/blogdescription", element: <BlogDescription /> },
+  { path: "/contact", element: <Contact /> },
+  { path: "/privacy", element: <Privacy /> },
+  { path: "/terms", element: <Terms /> },
+  { path: "/faqs", element: <FAQ /> },
+];
 
-// Rendring Routes
-
-export default function ThemeRoutes() {
-  return useRoutes(MainRoutes);
-}
+export default routes;
