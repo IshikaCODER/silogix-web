05ad57c40bf5cf6069009567c4508cf91c4aa27b
Ishika <ishika7473@gmail.com>
2024-08-04 21:56:50 +0530

    updated blog content

--- src/components/blog/BlogContent.js
+++ src/components/blog/BlogContent.js
@@ -5,7 +5,7 @@ import "../../styles/BlogContent.css";
 function BlogContent({ title, author, content }) {
   return (
     <div className="blog-content-area">
-      <div className="container">
+      <div className="container-md">
         <div className="blog-content-section">
           {/* <div className="blog-content-title">
             <h1 className="blog-title">{title}</h1>
